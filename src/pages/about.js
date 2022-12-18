d53f5d60cc32559d49a8470ff8e37b6d72128967
Tarmica <lilskyforever@gmail.com>
2022-12-19 00:52:00 +0200

    lets see

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>About Tarmica</h1>
+            <p1>Hi there! I am the not-so-proud creator of this site</p1>
+        </main>
+    )
+    }
+
+    const Head = () => 
+    <>
+        <title>About Page</title>
+        <meta name="description" content="This page is supposed to be speaking on about me. Hope I did justice"/>
+    </>
+    export { Head }
+    export default AboutPage
+
